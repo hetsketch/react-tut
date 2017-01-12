8ca076cad30f05eb49b61e16e1289aa44c164250
ibro <al3xander.koval@gmail.com>
2017-01-12 12:02:35 +0300

    create react component

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
+//React component
+var App = React.createClass({
+  render: function() {
+    return (
+        <div className="app">
+        Всем привет, я компонент App!
+        </div>
+      );
+  }
+});
+
 ReactDOM.render(
-  //prev version
-  //first argument - react component
-  // React.createElement('h1', null, 'Привет мир!'),
-  //place to insert
-  // document.getElementById('root')
-
-  //jsx version(markdown in JS)
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
-
 );
