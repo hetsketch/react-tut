1ba1d0c31ee887679225886e9ffe3bf4c3ac4cbe
ibro <al3xander.koval@gmail.com>
2017-01-12 11:46:38 +0300

    babel and JSX added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 ReactDOM.render(
-	//first argument - react component
-	React.createElement('h1', null, 'Привет мир!'),
-	//place to insert
-	document.getElementById('root')
+  //prev version
+  //first argument - react component
+  // React.createElement('h1', null, 'Привет мир!'),
+  //place to insert
+  // document.getElementById('root')
+
+  //jsx version(markdown in JS)
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+
 );
