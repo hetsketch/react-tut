9626b922c839eeaa5e8b84345986aad162d0fe1f
ibro <al3xander.koval@gmail.com>
2017-01-12 12:06:43 +0300

    added news component to app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
+//new component
+var News = React.createClass({
+  render: function() {
+    return (
+      <div className="news">
+      К сожаления, новостей нет.
+      </div>
+    );
+  }
+});
+
 //React component
 var App = React.createClass({
   render: function() {
     return (
         <div className="app">
         Всем привет, я компонент App!
+        //add News component to App
+        <News />
         </div>
       );
   }
